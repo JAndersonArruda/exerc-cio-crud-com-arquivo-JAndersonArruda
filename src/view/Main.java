7bf4b3a7257dfdf7eea866e961fac9796516ec4a
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2024-08-23 11:16:43 -0300

    test: run crud

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,31 @@
 package view;
 
+import dao.PessoaDao;
+import model.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PessoaDao pessoaDao = new PessoaDao();
+
+        Pessoa pessoa = new Pessoa("Miguel", "miguel2@gmail.com");
+
+        if (pessoaDao.savePessoa(pessoa)) {
+            System.out.println("Salvo com suceso!");
+        } else System.out.println("Falha ao salvar!");
+
+        System.out.println(pessoaDao.getPessoas());
+
+        if (pessoaDao.deletePessoa(pessoaDao.fetchPessoaEmail("miguel2@gmail.com"))) {
+            System.out.println("Deletado com suceso!");
+        } else System.out.println("Falha ao deletar!");
+
+        System.out.println(pessoaDao.getPessoas());
+
+        Pessoa pessoa1 = new Pessoa("Ana Beatriz", "ana@gmail.com");
+        if (pessoaDao.updatePessoa(pessoa1)) {
+            System.out.println("Atualização concluida!");
+        } else System.out.println("Falha na atualização!");
+
+        System.out.println(pessoaDao.getPessoas());
     }
 }
